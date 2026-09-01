4079de416fb8863db02d0231ff246f10e1d30de7
prestonaddy08 <pa513825@ohio.edu>
2026-09-01 17:48:54 -0400

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Preston Addy>
+ *        Date: <9/1/2026>
+ * Description: Git Lab 2 Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
